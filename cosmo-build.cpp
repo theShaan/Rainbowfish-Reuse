c49e6ac4906a8e921405e0a9ff69664134b20230
Mohammed (Shaan) Huzaifa Danish <shaan3@gmail.com>
2017-12-08 19:25:32 -0500

    Added reuse code

--- cosmo-build.cpp
+++ cosmo-build.cpp
@@ -32,6 +32,14 @@
 #include "debruijn_graph_shifted.hpp"
 #include "kmc_api/kmc_file.h"
 
+//REUSE INCLDUES
+#include <sparsepp/spp.h>
+using spp::sparse_hash_map;
+#include <boost/heap/priority_queue.hpp>  
+#include <boost/container/vector.hpp>
+#include <boost/dynamic_bitset.hpp>
+#include <vector>
+
 struct parameters_t {
   std::string input_filename = "";
   std::string output_prefix  = "";
@@ -91,7 +99,217 @@ int getMilliSpan(int nTimeStart){
   return nSpan;
 }
 
-int main(int argc, char* argv[]) {
+
+//REUSE BEGIN--------------------------------------------------------------
+
+inline void DumpIndexesToBV(boost::heap::priority_queue<uint64_t> &IdxList, boost::dynamic_bitset<uint64_t> &bits)
+{
+ using namespace boost::heap;
+  
+  if (IdxList.empty())
+    return;
+  
+    if (bits.size() < IdxList.top())
+     bits.resize(IdxList.top()); 
+  
+    while (!IdxList.empty())
+    {
+     uint64_t nIdx = IdxList.top();
+     IdxList.pop();
+     bits[nIdx] = 1;
+    }
+}
+
+inline void StaticToDynamicBV(const color_bv &old, boost::dynamic_bitset<uint64_t> &newbits)
+{
+    if (newbits.size() < old.size())
+      newbits.resize(old.size());
+  
+		for (unsigned int i = 0; i < old.size(); i++)
+		{
+			if (old[i] == 1)
+      		newbits[i] = 1;
+		}
+}
+
+inline void deserialize_color_bv(std::ifstream& colorfile, color_bv& value, size_t size) 
+{
+  colorfile.read((char*)&value, size);
+}	
+
+void ReuseColors(std::ifstream &colorfile, const char *kfile, size_t num_colors)
+{
+  using namespace boost::container;
+	//const int alphaSz = 4; //size of alphabet
+	const size_t cbv_size = sizeof(color_bv); // figure out if SHOULD be 32 or 752 or what
+
+	std::string kstr(kfile); //just check if we can open it
+	std::ifstream test(kstr);
+	if (!test.good())
+	{
+		cout << "Failed to open file " << kstr.c_str() << endl;
+		return;
+	}
+	test.close();
+	
+	clock_t timer = clock();
+	
+	cout << "Opening " << kstr.c_str() << endl;
+	
+  	debruijn_graph_shifted<> graph;
+  	load_from_file(graph, kstr);
+  
+	sparse_hash_map<uint64_t, uint64_t> colorMap; //<kmer, color> pairs
+	uint64_t colorIndex = num_colors;
+
+  cout << "Number of colors = " << num_colors << endl;
+  
+	//for each node in graph
+	for (size_t node = 0; node < graph.num_nodes(); node++)
+	{	
+		if ((node % 100000) == 0)
+		cout << node << " of " << graph.num_nodes() << endl;
+		
+		color_bv colorList;
+		boost::heap::priority_queue<uint64_t> newColors; //i changed this to a pq because we can quickly get the largest index for potential bitset resizing
+		boost::dynamic_bitset<> fullColors(colorIndex);
+		
+		for (unsigned long i = 1; i <= graph.sigma; i++) //for each possible alphabet symbol
+		{
+			ssize_t nextK; //like an index (color file output is in order)
+		
+			if ((nextK = graph.outgoing(node, i)) > -1) //collect out neighbors
+			{
+					colorfile.seekg(nextK*cbv_size);
+					color_bv value;
+					deserialize_color_bv(colorfile, value, cbv_size);
+					colorList |= value;
+				
+					auto cIterator = colorMap.find(nextK);
+				
+					if (cIterator != colorMap.end())
+						newColors.push(cIterator->second);
+			}
+
+			if ((nextK = graph.incoming(node, i)) > -1) //collect in neighbors
+			{
+					colorfile.seekg(nextK*cbv_size);
+					color_bv value;
+					deserialize_color_bv(colorfile, value, cbv_size);
+					colorList |= value;
+				
+					auto cIterator = colorMap.find(nextK);
+				
+					if (cIterator != colorMap.end())
+						newColors.push(cIterator->second);
+			}
+      
+		}
+		//dynamic BV <-- all color bits
+		DumpIndexesToBV(newColors, fullColors);
+		StaticToDynamicBV(colorList, fullColors);
+	
+    size_t idx = 0;
+    
+    while (idx < fullColors.size()) //search for an unused color
+    {
+      if (fullColors[idx] == 0)
+        break;
+      
+      idx++;
+    }
+
+    if (idx < fullColors.size())
+      colorMap[node] = idx;
+		
+		else colorMap[node] = colorIndex++; 
+	}
+	
+	std::string outfile(kfile);
+	outfile.append(".recolor");
+	std::ofstream ofs(outfile.c_str(), std::ofstream::out);
+	
+	if (!ofs.good())
+	{
+		std::cerr << "Failed to open file for output " << endl;
+		return;
+	}
+	
+	//we have all kmer<-->color info and  kmer,color,sources info
+	//at this point we have various options as to how to output this data
+	uint64_t count = 0;
+	
+	auto iterator = colorMap.begin();
+	
+	while (iterator != colorMap.end()) //iterate thru hash map
+	{		
+		if ((count++ % 100000) == 0)
+		{
+		cout << "Writing " << count << " of " << colorMap.size() << endl;	
+		}
+		
+		uint64_t kmer = iterator->first;
+		uint64_t color = iterator->second;
+		color_bv sources;
+		colorfile.seekg(kmer*cbv_size);
+		deserialize_color_bv(colorfile, sources, cbv_size);
+			
+		std::string ln = std::to_string(kmer);
+		ln.append(":");
+		ln.append(std::to_string(color));
+		ln.append(":");
+		
+		for (unsigned int i = 0; i < sources.size(); i++) //format is "kmer:color:source1,source2,...,sourcen/n"
+		{
+			if (sources[i] == 1)
+			{
+			ln.append(std::to_string(i));
+			ln.append(",");
+			}
+		}
+		
+		ln.append("/n");
+		
+		ofs << ln;
+		iterator++;
+	}
+	
+	ofs.close();
+	
+	clock_t end = clock();
+	double elapsed = double(end - timer)/CLOCKS_PER_SEC;
+	cout << "Completed (time = " << elapsed << ")" << endl;
+	cout << "New colors = " << colorIndex - num_colors << endl;
+	
+}
+//REUSE END-----------------------------------------------------------
+
+int main(int argc, char* argv[]) 
+{
+	if (argc > 4 && argv[1][1] == 'r')
+	{
+  COSMO_LOG(trace) << "Color reuse mode\n";
+  
+	std::ifstream colorfile(argv[2]);
+    
+	if (!colorfile.good())
+  {
+  cerr << "Could not open " << argv[2] << endl;
+	return 1;
+  }
+    
+  size_t colors = atoi(argv[4]);
+    
+  if (colors == 0)
+  {
+   cout  << "Bad color count " << endl;
+    return 0;
+  }
+    
+	ReuseColors(colorfile, argv[3], colors);	
+	return 0;
+	}
+
     int merge_done_time = 0;
     int start_time = getMilliCount();
   using namespace boost::adaptors;
@@ -172,7 +390,7 @@ int main(int argc, char* argv[]) {
       builder_t builder(params);
 
       COSMO_LOG(trace) << "Reading input and creating runs...";
-      for (auto & x : reader) {
+      for (auto & x : reader) { //looks like where each kmer is read
         builder.push(x);
       }
       // TODO: move dbg decl out of block and create copy constructor/operator etc
@@ -184,7 +402,7 @@ int main(int argc, char* argv[]) {
       typename stxxl::vector<uint8_t>::bufwriter_type * lcs_writer;
       size_t idx = 0;
       {
-        auto dbg = builder.build([&](size_t num_rows) {
+        auto dbg = builder.build([&](size_t num_rows) { //make the dbg from the reads
           //lcs_v = sdsl::int_vector<8>((params.variable_order)*num_rows);
           lcs_file.set_size((params.variable_order)*num_rows);
           // a bit hacky. Should seperate finding dummies from merging them in builder,
@@ -223,7 +441,8 @@ int main(int argc, char* argv[]) {
       delete lcs_writer;
       delete lcs_v;
 
-      if (params.variable_order) {
+      if (params.variable_order) 
+	  {
         sdsl::wt_int<sdsl::rrr_vector<63>> lcs_wt;
         // TODO: make SDSL accept a templated type for forward iteration
         sdsl::construct(lcs_wt, temp_lcs_file, 1);
@@ -268,7 +487,7 @@ int main(int argc, char* argv[]) {
     size_t max_union;
     uint32_t kmc_k;
     if ( !kmc_read_header(file_name, kmc_k, min_union, max_union, num_colors, kmer_data_bases) ) {
-      COSMO_LOG(error) << "Error reading databases listed in KMC2 list file" << file_name;
+      COSMO_LOG(error) << "Error reading databases listed in KMC2 list file" << file_name; //read header to get colors info
       exit(EXIT_FAILURE);
     }
     k = params.k = kmc_k;
@@ -287,6 +506,7 @@ int main(int argc, char* argv[]) {
 
     builder_t builder(params);
 
+    //the statement below is to read kmers from each source  (each source has one color) and build the color matrix
     size_t num_kmers_read = kmc_read_kmers(kmer_data_bases, k, [&](auto x, auto c) {
       builder.push(x,c);
     });
@@ -298,7 +518,7 @@ int main(int argc, char* argv[]) {
 
 
     ofstream cfs;
-    cfs.open(file_name + ".colors", ios::out | ios::binary);
+    cfs.open(file_name + ".colors", ios::out | ios::binary); //open filestream to output the color build
     
     // TODO: improve the memory use for colors here (can SDSL use external vectors?)
 //    bit_vector color_bv;
@@ -310,14 +530,14 @@ int main(int argc, char* argv[]) {
       //auto kmer = x.edge;
       //cerr << kmer_to_string(kmer,k) << endl;
       auto color = get<0>(x.payload);
-      serialize_color_bv(cfs, color);
-      for (size_t color_idx = 0; color_idx < num_colors; color_idx++) {
+      serialize_color_bv(cfs, color); //write color to stream ---
+      for (size_t color_idx = 0; color_idx < num_colors; color_idx++) { //go thru all colors and sum them to num_set ---
         // TODO: try complemented bits in color-major form in a *sd_vector* for large data set.
         //color_bv[color_idx * num_colors + edge_idx] = !color[color_idx];
 //        color_bv[edge_idx * num_colors + color_idx] = color[color_idx];
         num_set += color[color_idx];
       }
-      edge_idx++;
+      edge_idx++; //move to  next edge ---
     });
 
     COSMO_LOG(info) << "size of DBG: " << size_in_mega_bytes(dbg) << " MB";
